c6487f6ff385506e9b0c028e826fe1b40e0e8523
Arthur <arthurcapet@gmail.com>
2022-06-27 18:08:29 +0200

    update for required diags

--- diag.py
+++ diag.py
@@ -2,7 +2,7 @@
     The script will list all instances of *ptrc*.nc files and issue additional *diag*nc files with same filename structure and attributes.
     
     Args:
-        dir (str): directoy of NMO-PISCES outputs (default : './')
+        dir (str): directoy of NEMO-BAMHBI outputs (default : './')
         diaglist (list of str): identifiers of required diagnostics. Each should correspond to an entry in the diagnsotic dictionnary. 
 """ 
    # The main arguments is a directory.
@@ -14,18 +14,18 @@ from genericpath import isfile
 import os
 from glob import glob
 import xarray as xr
-import DiagFunctions_NEMOPISCES as diag
+import DiagFunctions_NEMOBAMHBI as diag
 
 # Arguments management #
 parser = argparse.ArgumentParser()
 parser.add_argument("-p","--printlist", help="Just print the list of available diagnostic definitions and required variables", action="store_true")
 parser.add_argument("-v","--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument('-d','--dir', type=str, default='./',
- help='Directory containing the NEMO-PISCES outputs. \n Variable requirements depends on diaglist, but we expect "ptrc","gridT", and "diad" files with the same filename structure')
+ help='Directory containing the NEMO-BAMHBI outputs. \n Variable requirements depends on diaglist, but we expect "ptrc_T","grid_T", etc.. files with the same filename structure')
 parser.add_argument('-k','--key', type=str, default='',
- help='Key required to be present in NEMO-PISCES ouptut filenames. If given the script will look for "*ptrc*KEY*nc" or "*KEY*ptrc*nc" files instead of simply "*ptrc*nc" ')
-parser.add_argument('-l','--diaglist', nargs='+', default=['TPPI','pocF100','TrophicEfficiencyI','zchlmax','CHLatMAX','ratioLargeM','CHLI','CHLM5','OMZextent','pocM200'],
- help= 'List of the diagnostics to be computed and stored in new *diag*.nc files.(eg. " ... -l TPPI pocF100 ")')
+ help='Key required to be present in NEMO-BAMHBI ouptut filenames. If given the script will look for "*ptrc*KEY*nc" or "*KEY*ptrc*nc" files instead of simply "*ptrc*nc" ')
+parser.add_argument('-l','--diaglist', nargs='+', default=['NPPO','ZooResp','DOC','ZooRespI','NPPOI','OXIDATIONBYDOXI','bac_oxygenconsumptionI'],
+ help= 'List of the diagnostics to be computed and stored in new *diag*.nc files.(eg. " ... -l ZooResp DOC")')
 
 args = parser.parse_args()
 indir =args.dir
@@ -43,47 +43,57 @@ if args.printlist:
 print('Selected Diags : ')
 diag.diaglist(dlist)
 
-
-
 # Setting up file lists #
-flist_p = glob(indir + '*ptrc*'+key+'*nc')
+flist_p = glob(indir + '*ptrc_T*'+key+'*nc')
 if not flist_p:
-    print("Couldn't any files of the form :"+indir + "*ptrc*"+key+"*nc")
-    flist_p = glob(indir +'*' +key+'*ptrc*nc')
+    print("Couldn't any files of the form :"+indir + "*ptrc_T*"+key+"*nc")
+    flist_p = glob(indir +'*' +key+'*ptrc_T*nc')
     if not flist_p:
-            print("Couldn't any files of the form :"+indir +'*' +key+'*ptrc*nc')
+            print("Couldn't any files of the form :"+indir +'*' +key+'*ptrc_T*nc')
 
-flist_d = [ f.replace('ptrc','diad') for f in flist_p ] 
-flist_g = [ f.replace('ptrc','gridT') for f in flist_p ] 
-flist_o = [ f.replace('ptrc','diag') for f in flist_p ] 
+# flist_p stand for : Pelagic Tracers on T grid
 
+# flist_b stand for : Benthic Tracers on T grid (2D)
+flist_b = [ f.replace('ptrc_T','btrc_T') for f in flist_p ]  
+
+# flist_u stand for : U velocity on U grid 
+flist_u = [ f.replace('ptrc_T','grid_U') for f in flist_p ] 
+
+# flist_v stand for : V velocity on V grid
+flist_v = [ f.replace('ptrc_T','grid_V') for f in flist_p ] 
+
+# flist_w stand for : vertical velocity on W grid
+flist_w = [ f.replace('ptrc_T','grid_W') for f in flist_p ] 
+
+# flist_g stand for Physical values on T grid 
+flist_g = [ f.replace('ptrc_T','grid_T') for f in flist_p ] 
+
+# flist_o is for the diagnostic ouptuts, on T grid 
+flist_o = [ f.replace('ptrc_T','diag_T') for f in flist_p ] 
 
 if args.verbose:
     print('Will take care of files : ')
     print(flist_p)
 
-#TODO test for alternate files (gridT, diad), depending on dependencies and provide meaningfull error message
-
-for i, (fp, fd, fg, fo) in enumerate(zip(flist_p, flist_d, flist_g, flist_o) ):
+for i, (fp, fb, fu, fv, fw, fg, fo) in enumerate(zip(flist_p, flist_b, flist_u, flist_v, flist_w, flist_g, flist_o) ):
     # 1: 'ptrc' files 
     x_p=xr.load_dataset(fp)
     xl=[x_p]
     # 2: 'diad' files 
+    '''
     if isfile(fd):
         x_d=xr.load_dataset(fd)
-        #FIXME TPP is now provided as time instant.
-        # For now I just overwrite and assume time_centered coordinates instead.
+        # FIXME consider time_instant and time_average variables ... 
         x_d=x_d.assign(time_counter=x_p['time_counter'].data )
         x_d=x_d.assign(time_counter_bounds=(('time_counter', 'axis_nbounds'), x_p['time_counter_bounds'].data) ) #FIXME MANIP on origin of time_counter
         xl.append(x_d)
-    # 3: 'gridT' files 
+    '''   # 3: 'gridT' files 
     if isfile(fg):
         x_g=xr.load_dataset(fg)
         xl.append(x_g)
 
     # Ensure we got all we may need
     x_a=xr.merge(xl)
-
 
     # Need to define a cell height variable to use xgcm 
     ### 16032022 AC - Had to replace 'depth_bounds' with x_a['deptht'].attrs['bounds']. 
@@ -105,6 +115,5 @@ for i, (fp, fd, fg, fo) in enumerate(zip(flist_p, flist_d, flist_g, flist_o) ):
         dlo=dlist
         
     bibi[dlist].to_netcdf(fo)
-
     print(fo + ' completed')
     del x_a
